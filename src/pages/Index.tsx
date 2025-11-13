79fec719cf7cdc994bb74a8c2282385b25e14692
WyvernPirate <moloieric80@gmail.com>
2025-11-13 15:46:45 +0200

    Fix import paths in Index component for consistency

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,12 +1,12 @@
 
 import React from "react";
-import Hero from "@/components/Hero";
-import Navbar from "@/components/Navbar";
-import AboutSection from "@/components/AboutSection";
-import SkillsSection from "@/components/SkillsSection";
-import ProjectsSection from "@/components/ProjectsSection";
-import ContactSection from "@/components/ContactSection";
-import Footer from "@/components/Footer";
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+import AboutSection from "../components/AboutSection";
+import SkillsSection from "../components/SkillsSection";
+import ProjectsSection from "../components/ProjectsSection";
+import ContactSection from "../components/ContactSection";
+import Footer from "../components/Footer";
 
 const Index = () => {
   return (
